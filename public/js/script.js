9f6b87decae585aa61f1507404debd07d60ad72c
Kishan Unnadkat <kunnadkat@Kishans-MacBook-Pro-2.local>
2017-06-04 18:51:01 +0100

    Added RockPaperScissorsScala to portfolio

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+$("#downloadCVBtn").click(function() {
+    window.location = 'misc/Kishans CV.pdf';
+});
+
+$("#DownloadRockPaperScissors").click(function() {
+    window.location = 'misc/RockPaperScissorsScala.zip';
+});
